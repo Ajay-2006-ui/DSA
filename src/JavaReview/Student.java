c8fa4e231853c8540212c64900b26155f955406f
Ajay <akanup24579@gmail.com>
2026-07-16 21:37:42 +0530

    Added static variable

--- src/JavaReview/Student.java
+++ src/JavaReview/Student.java
@@ -2,32 +2,17 @@ package JavaReview;
 
 public class Student {
     String name;
-    int age;
-     Student(String name, int age) {
-         this.name = name;
-         this.age = age;
-     }
-     void display() {
-         System.out.print(this.name+" ");
-         System.out.println(this.age);
-     }
-     void display(String title){
-         System.out.println(title);
-         System.out.println(this.name);
-         System.out.println(this.age);
-     }
-     void display(int copies){
-         for (int i = 0; i < copies; i++) {
-             System.out.println(this.name);
-             System.out.println(this.age);
-         }
-     }
-     public static void main(String[] args) {
-         Student st= new Student("Ajay",20);
-         st.display();
-         st.display("student details");
-         st.display(3);
-     }
-
-
+    static String College="VTU";
+    Student (String name){
+        this.name=name;
+    }
+    void display(){
+        System.out.println("Name: "+name+" "+College);
+    }
+    public static void main(String[] args) {
+        Student s1=new Student("Sam");
+        Student s2=new Student("Raj");
+        s1.display();
+        s2.display();
+    }
 }
