81de26905205895677153b68ca25a52f9579bc56
Ajay <akanup24579@gmail.com>
2026-08-31 13:27:39 +0530

    added lcs tabulaion code

---
+++ src/DSA/DPLCS.java
@@ -0,0 +1,25 @@
+package DSA;
+
+public class DPLCS {
+    static int lcs(String A,String B){
+        int [][]dp=new int[A.length()+1][B.length()+1];
+        for(int i=1;i<=A.length();i++){
+            for(int j=1;j<=B.length();j++){
+                if(A.charAt(i-1)==B.charAt(j-1)){
+                    dp[i][j]=dp[i-1][j-1]+1;
+                }
+                else{
+                    dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
+                }
+
+            }
+
+        }
+        return dp[A.length()][B.length()];
+    }
+    public static void main(String[] args) {
+        String A="abcde";
+        String B="ace ";
+        System.out.println(lcs(A,B));
+    }
+}
